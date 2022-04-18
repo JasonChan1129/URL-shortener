8f73ea6f2622864011f65bc03acf44386c3b9cb9
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-19 00:05:23 +0800

    refactor: move createRandomCode function to a seperate file

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const URL = require('../../models/url');
+const createRandomCode = require('../../createRandomCode');
 
 const router = express.Router();
 
@@ -24,52 +25,6 @@ router.post('/url', (req, res) => {
 				);
 			}
 		});
-	// find corresponding randomCode in mongoDB
-	// if found, return the randomCode
-	// if not found, create a randomCode, insert data to mongoDB
-	// render index with the randomCode
 });
-
-function createRandomCode() {
-	const upperCase = [
-		'A',
-		'B',
-		'C',
-		'D',
-		'E',
-		'F',
-		'G',
-		'H',
-		'I',
-		'J',
-		'K',
-		'L',
-		'M',
-		'N',
-		'O',
-		'P',
-		'Q',
-		'R',
-		'S',
-		'T',
-		'U',
-		'V',
-		'W',
-		'X',
-		'Y',
-		'Z',
-	];
-	const lowerCase = upperCase.map(item => item.toLowerCase());
-	const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-	const collections = [...upperCase, ...lowerCase, ...number];
-	const collectionsLength = collections.length;
-	let randomCode = '';
-	const randomCodeLength = 5;
-	for (let i = 0; i < randomCodeLength; i++) {
-		const randomIndex = Math.floor(Math.random() * collectionsLength);
-		randomCode += collections[randomIndex];
-	}
-	return randomCode;
-}
 
 module.exports = router;
