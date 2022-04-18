205f21a656b3820014ef2193d7016c0d919b4582
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-18 18:44:58 +0800

    feat: add generate random code function

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', () => console.log('db error!'));
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,73 @@
 const express = require('express');
+
+const URL = require('../../models/url');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.render('index');
 });
+
+router.post('/url', (req, res) => {
+	const url = req.body.url;
+	let randomCode = '';
+	URL.find({ URL: url })
+		.lean()
+		.then(data => {
+			if (data.length) {
+				randomCode = data.randomCode;
+				console.log(randomCode);
+			} else {
+				randomCode = createRandomCode();
+				URL.create({ URL: url, randomCode });
+			}
+		});
+	// find corresponding randomCode in mongoDB
+	// if found, return the randomCode
+	// if not found, create a randomCode, insert data to mongoDB
+	// render index with the randomCode
+});
+
+function createRandomCode() {
+	const upperCase = [
+		'A',
+		'B',
+		'C',
+		'D',
+		'E',
+		'F',
+		'G',
+		'H',
+		'I',
+		'J',
+		'K',
+		'L',
+		'M',
+		'N',
+		'O',
+		'P',
+		'Q',
+		'R',
+		'S',
+		'T',
+		'U',
+		'V',
+		'W',
+		'X',
+		'Y',
+		'Z',
+	];
+	const lowerCase = upperCase.map(item => item.toLowerCase());
+	const number = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+	const collections = [...upperCase, ...lowerCase, ...number];
+	const collectionsLength = collections.length;
+	let randomCode = '';
+	const randomCodeLength = 5;
+	for (let i = 0; i < randomCodeLength; i++) {
+		const randomIndex = Math.floor(Math.random() * collectionsLength);
+		randomCode += collections[randomIndex];
+	}
+	return randomCode;
+}
 
 module.exports = router;
