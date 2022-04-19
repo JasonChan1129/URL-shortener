276c4b0770aab213be7ea7a4497b20aeae87e22e
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-19 18:04:58 +0800

    docs: add comments

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,10 +4,12 @@ const URL = require('../../models/url');
 
 const router = express.Router();
 
+// handle index page
 router.get('/', (req, res) => {
 	res.render('index');
 });
 
+// handle redirect shorten url to full url
 router.get('/:randomCode', (req, res) => {
 	const { randomCode } = req.params;
 	URL.findOne({ randomCode: randomCode })
