29be64bd2dbd23e1b4874ac9e7dc781db7a697cf
Jason Chan <jasonchan19981017@hotmail.com>
2022-04-25 15:19:00 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server is listening on localhost:${PORT}`));
